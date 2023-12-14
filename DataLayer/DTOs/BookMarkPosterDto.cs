60ca017b67f56718cf8f8a2ee5b68c397cc3b43c
kraut <krautwald1337@gmail.com>
2023-12-14 14:46:22 +0100

    BookMarkPosterDto

---
+++ DataLayer/DTOs/BookMarkPosterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DTOs
+{
+    public class BookMarkPosterDto
+    {
+        public int UserId { get; set; }
+        public int BookmarkId { get; set; }
+        public string TitleId { get; set; }
+        public string? UserNote { get; set; }
+        public string? OmdbPoster { get; set; }
+        public string PrimaryTitle { get; set; }
+    }
+}
